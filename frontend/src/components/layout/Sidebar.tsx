71a377685592d11a6733265b7e57496a68b38ef2
abhaygunhalkar <abhaygunhalakr@gmail.comm>
2026-07-06 09:55:34 -0500

    Make FINAPP_AAG logo link to Dashboard

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -39,13 +39,13 @@ export default function Sidebar() {
         <div className="flex flex-col h-full">
           {/* Header */}
           <div className="flex items-center justify-between p-4 border-b border-white/10">
-            <h2 className="text-lg font-bold text-white flex items-center gap-2">
+            <NavLink to="/" className="text-lg font-bold text-white flex items-center gap-2 hover:text-sky-300 transition-colors">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-sky-400" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
                 <path fillRule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clipRule="evenodd" />
               </svg>
               FINAPP_AAG
-            </h2>
+            </NavLink>
             <button
               onClick={toggleSidebar}
               className="p-1 rounded-lg text-white/70 hover:bg-white/10 lg:hidden"
